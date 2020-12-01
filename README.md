1134c0cce287274962438a5fdac1e0f3f166d0d0
omerfinger@medflyt.com <omerfinger@medflyt.com>
2020-12-01 13:50:25 -0500

    first version of geocoder library

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # mf-google-geocoder
-JS library uses Google Geocoding API and parses the response into a flat, readable and minimal object.
+JS library that uses Google Geocoding API and parses the response into a flat, readable and minimal object.
---
+++ src/index.ts
@@ -0,0 +1,176 @@
+import * as googlemaps from '@googlemaps/google-maps-services-js';
+import { AddressType, GeocodingAddressComponentType } from '@googlemaps/google-maps-services-js';
+import * as https from "https";
+
+interface Geo {
+    lat: number;
+    lng: number;
+}
+
+export interface Options {
+    /** Google API key */
+    apiKey: string,
+    /** Indicates if to change the original address if fields are missing */
+    mfAutoFix?: boolean;
+}
+
+export interface GoogleGeoCodeResponse {
+    address_components: googlemaps.AddressComponent[];
+    formatted_address: string;
+    types: googlemaps.AddressType[];
+    place_id: string;
+    geometry: googlemaps.AddressGeometry;
+}
+
+export interface AddressDetails {
+    location: Geo;
+    country: string;
+    state: string;
+    county: string;
+    city: string;
+    street: string;
+    streetNumber: string;
+    zip: string;
+    fullAddress: string;
+    address2: string | null;
+    googleGeoCodeResponse: GoogleGeoCodeResponse;
+}
+
+class MissingAddressDetailsError extends Error {
+    constructor(public missingTypes: (AddressType | GeocodingAddressComponentType)[], public message: string) {
+        super(message);
+    }
+}
+
+export function parseGoogleGeoCodeToAddressDetails(googleGeoCode: GoogleGeoCodeResponse): AddressDetails {
+    const addressComponents = googleGeoCode.address_components;
+    const country: string | undefined =
+        addressComponents.find(
+            component => component.types.includes(AddressType.country) && component.types.includes(AddressType.political))?.short_name;
+    const state: string | undefined =
+        addressComponents.find(
+            component => component.types.includes(AddressType.administrative_area_level_1) && component.types.includes(AddressType.political))?.short_name;
+    const county: string | undefined = addressComponents.find(
+        component => component.types.includes(AddressType.administrative_area_level_2) && component.types.includes(AddressType.political))?.short_name;
+    const city: string | undefined =
+        (addressComponents.find(
+            component => component.types.includes(AddressType.neighborhood) && component.types.includes(AddressType.political))?.short_name)
+        ?? (addressComponents.find(
+            component => component.types.includes(AddressType.sublocality) && component.types.includes(AddressType.political))?.short_name);
+    const street: string | undefined = addressComponents.find(
+        component => component.types.includes(AddressType.route))?.short_name;
+    const streetNumber: string | undefined = addressComponents.find(
+        component => component.types.includes(GeocodingAddressComponentType.street_number))?.short_name;
+    const zip: string | undefined = addressComponents.find(
+        component => component.types.includes(AddressType.postal_code))?.short_name;
+    const address2: string | undefined = addressComponents.find(
+        component => component.types.includes(AddressType.subpremise))?.short_name;
+    const fullAddress: string = googleGeoCode.formatted_address;
+
+    if (country === undefined) {
+        throw new MissingAddressDetailsError([AddressType.country, AddressType.political], `Missing  ${AddressType.political} and ${AddressType.country} types in address components`)
+    }
+    if (state === undefined) {
+        throw new MissingAddressDetailsError([AddressType.administrative_area_level_1, AddressType.political], `Missing ${AddressType.political} and  ${AddressType.administrative_area_level_1} types in address components`)
+    }
+    if (county === undefined) {
+        throw new MissingAddressDetailsError([AddressType.administrative_area_level_2, AddressType.political], `Missing political ${AddressType.political} and  ${AddressType.administrative_area_level_2} types in address components`)
+    }
+    if (city === undefined) {
+        throw new MissingAddressDetailsError([AddressType.neighborhood, AddressType.political, AddressType.sublocality], `Missing  ${AddressType.sublocality}, ${AddressType.neighborhood} and ${AddressType.sublocality}types in address components`)
+    }
+    if (street === undefined) {
+        throw new MissingAddressDetailsError([AddressType.route], `Missing ${AddressType.route} type in address components`)
+    }
+    if (streetNumber === undefined) {
+        throw new MissingAddressDetailsError([GeocodingAddressComponentType.street_number], `Missing ${GeocodingAddressComponentType.street_number} type in address components`)
+    }
+    if (zip === undefined) {
+        throw new MissingAddressDetailsError([AddressType.postal_code], `Missing ${AddressType.postal_code} type in address components`)
+    }
+
+    return {
+        streetNumber: streetNumber,
+        street: street,
+        city: city,
+        county: county,
+        state: state,
+        country: country,
+        zip: zip,
+        fullAddress: fullAddress,
+        address2: address2 !== undefined ? `#${address2}` : null,
+        googleGeoCodeResponse: googleGeoCode,
+        location: googleGeoCode.geometry.location
+    }
+}
+
+export async function getAddressDetails(addressText: string, options: Options): Promise<AddressDetails> {
+    const streetNumbersRegex = /^([0-9]+)(\s)([0-9]+ )/;
+    let modifiedAddressText = addressText.replace(streetNumbersRegex, "$1-$3");
+    let googleResponse = await getGoogleGeoCode(modifiedAddressText, options);
+    try {
+        return parseGoogleGeoCodeToAddressDetails(googleResponse);
+    }
+    catch (e) {
+        if (e instanceof MissingAddressDetailsError) {
+            if (e.missingTypes.includes(AddressType.administrative_area_level_2)) {
+                const streetNumberRegex = /^[0-9]+\s|^[0-9]+-[0-9]+\s/;
+                modifiedAddressText = modifiedAddressText.replace(streetNumberRegex, '');
+                googleResponse = await getGoogleGeoCode(modifiedAddressText, options);
+                return parseGoogleGeoCodeToAddressDetails(googleResponse);
+            } else {
+                throw e;
+            }
+        } else {
+            throw e;
+        }
+    }
+}
+
+function getGoogleGeoCode(addressText: string, options: Options): Promise<GoogleGeoCodeResponse> {
+    return new Promise<GoogleGeoCodeResponse>((resolve, reject) => {
+        const req = https.request(
+            {
+                host: "maps.googleapis.com",
+                hostname: "maps.googleapis.com",
+                port: 443,
+                method: "GET",
+                path:
+                    "/maps/api/geocode/json?address=" +
+                    encodeURIComponent(addressText) +
+                    options.apiKey
+            },
+            (response) => {
+                if (
+                    response.statusCode === undefined ||
+                    response.statusCode < 200 ||
+                    response.statusCode > 300
+                ) {
+                    reject(new Error(`Status code error: ${response.statusCode}`));
+                }
+                let body = "";
+                response.on("data", (d) => {
+                    body += d;
+                });
+                response.on("end", () => {
+                    const googleAddressComponents = JSON.parse(body);
+                    if (googleAddressComponents.results.length === 0) {
+                        throw new Error("Address not found, check validity of address");
+                    }
+                    const result: GoogleGeoCodeResponse = {
+                        address_components: googleAddressComponents.results[0].address_components,
+                        formatted_address: googleAddressComponents.results[0].formatted_address,
+                        geometry: googleAddressComponents.results[0].geometry,
+                        place_id: googleAddressComponents.results[0].place_id,
+                        types: googleAddressComponents.results[0].types
+                    }
+                    resolve(result);
+                });
+            }
+        );
+        req.on("error", (err) => {
+            reject(err);
+        });
+        req.end("", "utf8");
+    });
+}

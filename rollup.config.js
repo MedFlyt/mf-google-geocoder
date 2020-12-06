ada06409bd163ecccff203c8ef973d9929e443ee
omerfinger@medflyt.com <omerfinger@medflyt.com>
2020-12-06 11:23:48 -0500

    commonjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,15 @@
 import pkg from './package.json';
 import nodePolyfills from 'rollup-plugin-node-polyfills';
 import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 export default {
     input: 'build/index.js',
     output: [
         {
             file: "./bundle/mf-geo-coder.js",
-            format: 'umd',
-            name: 'mfGeoCoder',
-            globals: {
-                "@googlemaps/google-maps-services-js": 'google_maps_services_js_1'
-            }
+            format: 'iife',
+            name: 'mfGeoCoder'
+
         }
     ],
     external: [
@@ -18,6 +17,9 @@ export default {
     ],
     plugins: [
         nodePolyfills(),
-        commonjs()
+        commonjs({
+            include: 'node_modules/**'
+        }),
+        nodeResolve({ browser: true })
     ]
 };

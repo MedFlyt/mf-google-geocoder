1d3947901fdcb59e6bca82645a30c5b3132dfe59
Newbie012 <co.eliya2@gmail.com>
2020-12-16 19:18:47 +0200

    Remove webpack. use tsc instead

---
+++ bundle/index.d.ts
@@ -0,0 +1,55 @@
+import { AddressComponent, AddressGeometry, AddressType, GeocodingAddressComponentType, Status } from '@googlemaps/google-maps-services-js';
+interface Geo {
+    lat: number;
+    lng: number;
+}
+export interface Options {
+    /** google API key */
+    apiKey: string;
+    /** indicates if to change the original address if fields are missing */
+    mfAutoFix?: boolean;
+}
+export interface GoogleGeoCodeResponse {
+    address_components: AddressComponent[];
+    formatted_address: string;
+    types: AddressType[];
+    place_id: string;
+    geometry: AddressGeometry;
+    status: Status;
+}
+export interface AddressDetails {
+    /** geometry.location */
+    location: Geo;
+    /** address component with 'country' and 'political' types */
+    country: string;
+    /** address component with 'administrative_area_level_1' and 'political' types */
+    state: string;
+    /** address component with 'administrative_area_level_2' and 'political' types */
+    county: string;
+    /** address component with 'neighborhood \ sublocality \ locality' and 'political' types */
+    city: string;
+    /** address component with 'route' type */
+    street: string;
+    /** address component with 'street_number' type */
+    streetNumber: string | null;
+    /** address component with 'postal_code' type */
+    zip: string;
+    /** address component with 'postal_code_suffix' type */
+    zipSuffix: string | null;
+    /** formatted_address */
+    fullAddress: string;
+    /** address component with 'subpremise' type */
+    address2: string | null;
+    /** original google status */
+    status: Status;
+    /** original google API response */
+    googleGeoCodeResponse: GoogleGeoCodeResponse;
+}
+export declare class MissingAddressDetailsError extends Error {
+    missingTypes: (AddressType | GeocodingAddressComponentType)[];
+    message: string;
+    constructor(missingTypes: (AddressType | GeocodingAddressComponentType)[], message: string);
+}
+export declare const fromGoogleGeoCode: (googleGeoCodeResponse: GoogleGeoCodeResponse, options: Options) => Promise<AddressDetails>;
+export declare const fromAddressText: (addressText: string, options: Options) => Promise<AddressDetails>;
+export {};
